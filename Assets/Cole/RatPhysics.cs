26f54f76350d1e0389beb118aeb747d1a0712bb6
Cole Druffel <Druffel12@gmail.com>
2018-07-23 17:32:07 -0700

    push dev pre merge

---
+++ Assets/Cole/RatPhysics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatPhysics : MonoBehaviour
+{
+    static bool Ragdoll;
+    Rigidbody[] RB;
+
+	void Start ()
+    {
+        Ragdoll = false;
+	}
+	
+	void Update ()
+    {
+
+        if (Ragdoll == true)
+        {
+            Fired();
+        }
+    }
+
+    void Fired()
+    {
+        GameObject[] BodyParts = GameObject.FindGameObjectsWithTag("Rat");
+        RB = new Rigidbody[BodyParts.Length];
+        for (int i = 0; i < RB.Length; i++)
+        {
+            RB[i] = BodyParts[i].GetComponent<Rigidbody>();
+        }
+
+    }
+}
