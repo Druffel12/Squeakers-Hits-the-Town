6b3ca4ce90a4d04ddb7daffd892dd08b1d036ff3
Cole Druffel <Druffel12@gmail.com>
2018-07-24 12:10:02 -0700

    merge for project construction

---
+++ Assets/Cole/Scripts/DebrisManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisManager : MonoBehaviour
+{
+
+    IEnumerator ClearDebris()
+    {
+        //GameObject.FindGameObjectsWithTag("Debris");
+        yield return new WaitForSeconds(10.0f);
+        Destroy(gameObject);
+        yield return null;
+
+    }
+}
