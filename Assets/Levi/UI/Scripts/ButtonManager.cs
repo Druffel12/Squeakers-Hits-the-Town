629ac2d7348e5a6ee570b51bda3ca96cf6cf660c
Levi Rosenberg <levirosenberg97@gmail.com>
2018-07-23 17:30:32 -0700

    first commit

---
+++ Assets/Levi/UI/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadLevel(string wool)
+    {
+        SceneManager.LoadScene(wool);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Levi/UI/Scripts/ShopScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopScript : MonoBehaviour
+{
+    public float score;
+
+    public float fadeOutTime;
+
+    public Text text;
+
+    Color textColor = new Color(1f, 0f, 0f);
+
+    bool startFade;
+
+    private void Start()
+    {
+        startFade = false;
+    }
+
+    public void Buy(int price)
+    {
+        if (score >= price)
+        {
+            score -= price;
+        }
+        else
+        {
+            text.color = textColor;
+            startFade = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(startFade == true)
+        {
+            text.color = Color.Lerp(text.color, Color.clear, fadeOutTime * Time.deltaTime);
+        }
+        if(text.color == Color.clear)
+        {
+            startFade = false;
+        }
+    }
+
+}
